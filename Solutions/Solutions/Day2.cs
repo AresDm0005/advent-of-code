39665838f3b1e362a94ed80d1888dead75ed8bf5
Дмитриев Арсений <Arseniy.Dmit-vk@yandex.ru>
2022-12-03 01:48:37 +0500

    day-2 completed

---
+++ Solutions/Solutions/Day2.cs
@@ -0,0 +1,65 @@
+﻿namespace Solutions;
+
+public class Day2
+{
+    public static readonly string InputFile = "Day2-1.txt";
+
+    public static readonly Dictionary<char, int> ItemScores = new()
+    {
+        { 'X', 1 },
+        { 'Y', 2 },
+        { 'Z', 3 },
+    };
+    public static readonly Dictionary<char, char> WinningCombinations = new()
+    {
+        { 'X', 'C' },
+        { 'Y', 'A' },
+        { 'Z', 'B' },
+        { 'A', 'Z' },
+        { 'B', 'X' },
+        { 'C', 'Y' },
+    };
+
+    public static readonly Dictionary<char, int> ResultToScores = new()
+    {
+        { 'X', 0 },
+        { 'Y', 3 },
+        { 'Z', 6 },
+    };
+
+    public static int FirstStar()
+    {
+        var answer = File.ReadAllLines(InputFinder.GetInputPath(InputFile))
+            .Select(x => x.Split())
+            .Sum(x => 
+                ItemScores[x[1][0]] +
+                (WinningCombinations[x[1][0]] == x[0][0]
+                    ? 6
+                    : (WinningCombinations[x[0][0]] == x[1][0] ? 0 : 3)));
+
+        return answer;
+    }
+
+    public static int SecondStar()
+    {
+        var answer = File.ReadAllLines(InputFinder.GetInputPath(InputFile))
+            .Select(x => x.Split())
+            .Sum(x => 
+            {
+                var options = new HashSet<char> { 'X', 'Y', 'Z' };
+                var answer = x[1][0] switch
+                {
+                    'X' => WinningCombinations[x[0][0]],
+                    'Y' => x[0][0] + 'X' - 'A',
+                    'Z' => WinningCombinations
+                        .FirstOrDefault(y => y.Value == x[0][0])
+                        .Key,
+                    _ => '0'
+                };
+
+                return ItemScores[(char)answer] + ResultToScores[x[1][0]];
+            });
+
+        return answer;
+    }
+}
--- Solutions/Solutions/Program.cs
+++ Solutions/Solutions/Program.cs
@@ -5,5 +5,7 @@ PrintAnswers();
 void PrintAnswers()
 {
     Console.WriteLine($"D1S1: {Day1.FirstStar()} calories for top-1");
-    Console.WriteLine($"D1S2: {Day1.SecondStar()} calories for top-3");
+    Console.WriteLine($"D1S2: {Day1.SecondStar()} calories for top-3");    
+    Console.WriteLine($"D2S1: {Day2.FirstStar()} points of score");
+    Console.WriteLine($"D2S2: {Day2.SecondStar()} points of score");
 }
